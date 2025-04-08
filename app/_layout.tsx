8b38b5693a69054279c875dc5d728e166de3da4e
jcurbelo <robin@paella.dev>
2025-04-08 11:22:41 -0400

    adds crossmint wallet provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,15 +3,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import utilities
 import "@/src/utils";
+import WalletProvider from "@/app/components/providers/WalletProvider";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "Crossmint Expo Demo" }} />
-      </Stack>
+      <WalletProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{ title: "Crossmint Expo Demo" }}
+          />
+        </Stack>
+      </WalletProvider>
     </QueryClientProvider>
   );
 }
